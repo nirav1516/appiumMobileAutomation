26e9f4dff0a6802497599ff1f5e77c23a6d91be8
Nirav Shah <nirav1516@gmail.com>
2021-01-04 02:57:20 -0800

    Basics of Page Object Model

--- appiumrefresh/src/testcases/Basic.java
+++ appiumrefresh/src/testcases/Basic.java
@@ -9,6 +9,9 @@ import org.openqa.selenium.By;
 import init.Setup;
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import pageobjects.HomePage;
+import pageobjects.PreferenceDependenciesPage;
+import pageobjects.PreferencePage;
 
 public class Basic extends Setup{
 
@@ -17,24 +20,30 @@ public class Basic extends Setup{
 		AndroidDriver<AndroidElement> driver = capabilities();
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		
-		AndroidElement e=  driver.findElementByXPath("//android.widget.TextView[@text='Preference']");
+		HomePage hP = new HomePage(driver);
+		PreferencePage pP = new PreferencePage(driver);
+		PreferenceDependenciesPage pDP = new PreferenceDependenciesPage(driver);
+		
+		AndroidElement e;
+		hP.clickPreferenceMenu();
+		
+		
+		
+		e=  pP.DependeciesMenu;
 		e.click();
 		
-		e=  driver.findElementByXPath("//android.widget.TextView[@content-desc='3. Preference dependencies']");
+		e = pDP.wifiCheckBox;
 		e.click();
 		
-		e = driver.findElementById("android:id/checkbox");		
+		e=  pDP.wifiSettingsMenu;//driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]"));//driver.findElementByXPath("(//android.widget.RelativeLayout)[2]");
 		e.click();
 		
-		e=  driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]"));//driver.findElementByXPath("(//android.widget.RelativeLayout)[2]");
-		e.click();
-		
-		e = driver.findElement(By.className("android.widget.EditText"));
+		e = pDP.wifiTextBox;//driver.findElement(By.className("android.widget.EditText"));
 		e.setValue("Nirav");
 		 
 		//e= driver.findElement(By.id("android:id/button1"));
 		
-		List<AndroidElement> eL = driver.findElementsByClassName("android.widget.Button");
+		List<AndroidElement> eL = pDP.settingButtons;//driver.findElementsByClassName("android.widget.Button");
 		e = eL.get(1);
 		e.click();
 	}
